5705aee88494cd02a7a098539eb32af239ea78eb
Daniel Kary <dankary@gmail.com>
2020-03-23 18:14:30 -0400

    template adjustments

--- R/template.R
+++ R/template.R
@@ -5,17 +5,27 @@
 #' This is intended to be run before data processing for a new state begins.
 #' 
 #' @param state character: Two letter state designation
-#' @param time_period character: Time period for the first dashboard 
+#' @param period character: Time period for the first dashboard 
 #' (e.g., "2019-q4", "2020-q4", etc.)
-#' @param sa_path character: File path to the Southwick main folder 
-#' (for analysis and data, etc.)
+#' @param analysis_path character: File path for the analysis
+#' @param sensitive_path character: File path for the sensitive data. If NULL,
+#' no folders will be created.
+#' @param production_path character: File path for the production data. If NULL, 
+#' no folders will be created.
 #' @family functions for making template files/folders
 #' @export
 #' @examples
-#' # new_dashboard("YY", "2018-q4")
-new_dashboard <- function(state, time_period, sa_path = "E:/SA/") {
-    # error handling to avoid running if relevant folders already exist
-    analysis_path <- file.path(sa_path, "Projects", "Data-Dashboards", state, time_period)
+#' # new_dashboard("YY", "2019-q4")
+new_dashboard <- function(
+    state, period, 
+    analysis_path = file.path("E:/SA/Projects/Data-Dashboards", state, period),
+    sensitive_path = file.path("E:/SA/Data-Sensitive/Data-Dashboards", state, paste0("raw-", period)),
+    production_path = file.path("E:/SA/Data-Production/Data-Dashboards", state)
+) {
+    # error - don't run if a directory with that time period already exists
+    if (dir.exists(analysis_path)) {
+        stop("That period already exists!: ", analysis_path, call. = FALSE)
+    }
     dir.create(analysis_path, recursive = TRUE)
     
     # create analysis folders/files
@@ -26,6 +36,32 @@ new_dashboard <- function(state, time_period, sa_path = "E:/SA/") {
         file.copy(i, analysis_path, recursive = TRUE, overwrite = FALSE)
     }
     
+    # - rename 2 analysis files
+    file.rename(
+        file.path(analysis_path, "Rprofile-tmp"),
+        file.path(analysis_path, ".Rprofile")
+    )
+    file.rename(
+        file.path(analysis_path, "template.Rproj"),
+        file.path(analysis_path, paste0(state, "-", period, ".Rproj"))
+    )
     
-    # create data folders/files
+    # - replace parameter (state, period) values in params.R
+    f <- file.path(analysis_path, "params.R")
+    x <- readLines(f)
+    x <- gsub("__state__", state, x)
+    x <- gsub("__period__", period, x)
+    cat(x, file = f, sep = "\n")
+    
+    # create data folders (sensitive, production)
+    dir_create <- function(path) {
+        if (!is.null(path)) {
+            dir.create(path, recursive = TRUE, showWarnings = FALSE)
+        }
+    }
+    dir_create(sensitive_path)
+    dir_create(production_path)
+    
+    # print message
+    message("A new dashboard project has been initialized:\n  ", analysis_path)
 }
--- inst/template/params.R
+++ inst/template/params.R
@@ -1,20 +1,22 @@
 # state/period-specific parameters to be called in production code
 
-state <- "XX"
-period <- "yyyy-qn"
-firstyr <- 2010 # first year to include in dashboard results
+state <- "__state__"
+period <- "__period__"
 
+# for data processing
+dir_production <- "E:/SA/Data-production/Data-Dashboards"
 dir_sensitive <- "E:/SA/Data-sensitive/Data-Dashboards"
 dir_raw <- file.path(dir_sensitive, state, paste0("raw-", period))
+
 db_raw <- file.path(dir_sensitive, state, paste0("raw-", period, ".sqlite3"))
 db_standard <- file.path(dir_sensitive, state, paste0("standard-", period, ".sqlite3"))
-
-dir_production <- "E:/SA/Data-production/Data-Dashboards"
 db_license <- file.path(dir_production, state, "license.sqlite3")
+
+# for building license histories & dashboard summaries
 db_history <- file.path(dir_production, state, "history.sqlite3")
 db_census <- file.path(dir_production, "_Shared", "census.sqlite3")
 
-# for building license histories & dashboard summaries
+firstyr <- 2010 # first year to include in dashboard results
 lastyr <- as.integer(substr(period, 1, 4))
 quarter <- as.integer(substr(period, 7, 7))
 yrs <- firstyr:lastyr
