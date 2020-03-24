d5c3cef3b22a8dd47b8fd282cbee20b6edb9e0de
Daniel Kary <dankary@gmail.com>
2020-03-24 18:47:27 -0400

    template work

--- inst/template/code/1-prep-license-data/01-load-raw.R
+++ inst/template/code/1-prep-license-data/01-load-raw.R
@@ -2,48 +2,60 @@
 
 library(tidyverse)
 library(DBI)
-library(salic)
+library(salicprep)
 
 source("code/params.R")
-files <- list.files(dir_raw)
+files <- list.files(dir_raw, full.names = TRUE)
 
-lic_file <- files[1] ## state-specific
-cust_file <- files[2] ## state-specific
-sale_file <- files[3] ## state-specific
+## state-specific file identification
+lic_file = files[1]
+cust_file = files[2]
+sale_file = files[3]
 
-## all the read functions for pulling in data will be state-specific as well
+# Load Customers ----------------------------------------------------------------
 
-# License -----------------------------------------------------------
+# check format for import
+read_lines(cust_file, n_max = 2)
 
-f <- file.path(dir_raw, lic_file)
-lic <- f %>%
-    read_csv() %>%
-    mutate(raw_lic_id = row_number())
-glimpse(lic)
-
-# Customer ----------------------------------------------------------------
-
-f <- file.path(dir_raw, cust_file)
-cust <- f %>% read_csv(
+## note: all the read functions for pulling in data will be state-specific
+cust <- read_delim(
+    cust_file,
     progress = FALSE,
     col_types = cols(.default = col_character())
-) %>%
-    mutate(raw_cust_id = row_number())
-salic::count_lines_textfile(f) == nrow(cust) + 1
+)
+problems(cust) # looking for import problems
+check_raw_lines(cust, cust_file) # difference in row counts should usually be 1
+
+cust <- mutate(cust, raw_cust_id = row_number())
 glimpse(cust)
 
-# Sale --------------------------------------------------------------------
+# Load Sales --------------------------------------------------------------------
 
-f <- file.path(dir_raw, sale_file)
-sale <- f %>% read_csv(
+read_lines(sale_file, n_max = 2)
+
+sale <- read_delim(
+    sale_file,
     progress = FALSE,
-    col_types = cols(.default = col_character(), LicenseYear = col_integer())
-) %>%
-    mutate(raw_sale_id = row_number())
-salic::count_lines_textfile(f) == nrow(sale) + 1
+    col_types = cols(.default = col_character())
+)
+problems(sale)
+check_raw_lines(sale, sale_file)
+
+sale <- mutate(sale, raw_sale_id = row_number())
 glimpse(sale)
 
-# To Sqlite ---------------------------------------------------------------
+# Load License Types -----------------------------------------------------------
+
+read_lines(lic_file, n_max = 2)
+
+lic <- read_csv(lic_file, col_types = cols(.default = col_character))
+problems(lic)
+check_raw_lines(cust, cust_file)
+
+lic <- mutate(lic, raw_lic_id = row_number())
+glimpse(lic)
+
+# Write To Sqlite ---------------------------------------------------------------
 
 con <- dbConnect(RSQLite::SQLite(), db_raw)
 dbWriteTable(con, "lic", lic, overwrite = TRUE)
