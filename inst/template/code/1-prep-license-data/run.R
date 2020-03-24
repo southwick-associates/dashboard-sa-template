59204f49b088d3b17d50d8d62abfb5d126a8b82f
Daniel Kary <dankary@gmail.com>
2020-03-24 16:08:40 -0400

    template workflow tweaking

--- inst/template/code/1-prep-license-data/run.R
+++
@@ -1,7 +0,0 @@
-# run scripts and save log files to html
-
-# prepare production data
-workflow::run_html("code/1-prep-license-data/01-load-raw.R")
-workflow::run_html("code/1-prep-license-data/02-standardize.R")
-workflow::run_rmd_html("code/1-prep-license-data/03-check-initial.Rmd")
-workflow::run_html("code/1-prep-license-data/04-finalize.R")
---
+++ inst/template/code/run.R
@@ -0,0 +1,10 @@
+# run scripts and save log files to html
+
+# prepare production data
+workflow::run_html("code/1-prep-license-data/01-load-raw.R")
+workflow::run_html("code/1-prep-license-data/02-standardize.R")
+# note: some manual editing may be needed for preparing license types
+workflow::run_html("code/1-prep-license-data/03-prep-lic-types.R")
+workflow::run_rmd_html("code/1-prep-license-data/04-check-initial.Rmd")
+workflow::run_html("code/1-prep-license-data/05-finalize.R")
+workflow::run_rmd_html("code/1-prep-license-data/06-check-final.Rmd")
