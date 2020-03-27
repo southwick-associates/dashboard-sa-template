8ff4809f8c0e2ea9c9707268d7988e2af955a0a5
Daniel Kary <dankary@gmail.com>
2020-03-27 17:07:45 -0400

    template work

---
+++ inst/template/code/2-prep-dashboard/02-dashboard-data.R
@@ -0,0 +1,4 @@
+# prepare dashboard summaries
+# - also check summaries with a shiny app: dashtemplate::run_visual()
+
+library(dashtemplate)
--- inst/template/code/run.R
+++ inst/template/code/run.R
@@ -7,3 +7,7 @@ workflow::run_html("code/1-prep-license-data/03-prep-lic-types.R")
 workflow::run_rmd_html("code/1-prep-license-data/04-check-initial.Rmd")
 workflow::run_html("code/1-prep-license-data/05-finalize.R")
 workflow::run_rmd_html("code/1-prep-license-data/06-check-final.Rmd")
+
+# prepare dashboard summary data
+workflow::run_html("code/2-prep-dashboard/01-history.R")
+workflow::run_html("code/2-prep-dashboard/02-dashboard-data.R")
