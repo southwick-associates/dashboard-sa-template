c6ef75647c53d37cfebdbf7f2173b3278bf5eef3
Daniel Kary <dankary@gmail.com>
2020-04-13 11:11:51 -0400

    template work

--- inst/template-individual/code/run.R
+++ inst/template-individual/code/run.R
@@ -2,11 +2,13 @@
 
 # prepare production data
 workflow::run_html("code/1-prep-license-data/01-load-raw.R")
+workflow::run_html("code/1-prep-license-data/02a-lic-types.R")
 workflow::run_html("code/1-prep-license-data/02-standardize.R")
-workflow::run_html("code/1-prep-license-data/03-prep-lic-types.R")
-workflow::run_rmd_html("code/1-prep-license-data/04-check-initial.Rmd")
-workflow::run_html("code/1-prep-license-data/05-finalize.R")
-workflow::run_rmd_html("code/1-prep-license-data/06-check-final.Rmd")
+workflow::run_html("code/1-prep-license-data/03-pull-geocode.R")
+workflow::run_html("code/1-prep-license-data/04-prep-lic-types.R")
+workflow::run_rmd_html("code/1-prep-license-data/05-check-initial.Rmd")
+workflow::run_html("code/1-prep-license-data/06-finalize.R")
+workflow::run_rmd_html("code/1-prep-license-data/07-check-final.Rmd")
 
 # build license history
 workflow::run_html("code/2-license-history/1-run-history.R")
